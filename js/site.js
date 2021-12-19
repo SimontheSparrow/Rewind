cdd28e8f62810137e2cb05eaec9f2b3263d96e83
SimontheSparrow <88796995+SimontheSparrow@users.noreply.github.com>
2021-12-19 15:46:45 +0100

    done

--- js/site.js
+++ js/site.js
@@ -1,3 +1,24 @@
-function HelloWorld() {
-    alert("Hello world");
+function getString() {
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+function reverseString(userString) {
+    let revString = [];
+
+    for (let i = userString.length - 1; i >= 0; i--) {
+
+        revString += userString[i];
+    }
+    return revString;
+
+}
+
+function displayString(revString) {
+    document.getElementById("msg").innerHTML = `${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
